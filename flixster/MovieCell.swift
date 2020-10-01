4845c03cab75a9cdbd78ded0b2ceb810939efcae
Francisco Rodriguez <francisco37.fr@gmail.com>
2020-09-30 21:03:27 -0700

    Initial Commit

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Carina Rodriguez on 9/23/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLable: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
